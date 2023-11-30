ec5df158003edc56b9156c4ad0d65d317af8c798
Klayze <M.Bajzek@gsi.de>
2023-11-30 18:43:17 +0100

    fixed testcases

--- exercises/3_modules/modules_1.bash
+++ exercises/3_modules/modules_1.bash
@@ -1,7 +1,7 @@
 while [[ 1 ]]; do
 	read_input
 	IFS=',' read -r a1 a2 a3 a4 <<< $answer
-	if [[ "x"$a1 == "x1"  && "x"$a2 == "x1" && "x"$a3 == "x17" && "x"$a4 == "x4" ]]; then
+	if [[ "x"$a1 == "x0"  && "x"$a2 == "x0" && "x"$a3 == "x16" && "x"$a4 == "x3" ]]; then
 		success
 		break
 	else
